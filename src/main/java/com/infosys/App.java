50f39a35eedc224cc51fed5a6c4b03579a6145ac
Koushikpai <koushikpai6080@gmail.com>
2024-11-06 10:08:33 +0530

    Sneak Directions Intial

---
+++ src/main/java/com/infosys/App.java
@@ -0,0 +1,11 @@
+package com.infosys;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Sneak Is Moving to the right---->");
+        System.out.println("Hello Snaek");
+    }
+}
